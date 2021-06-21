016eeb4390838e3ab35a5764f92c7484d969c9eb
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2021-06-21 21:46:54 +0900

    ♻️ メイン部分をコンポーネント化

---
+++ components/Main.js
@@ -0,0 +1,14 @@
+import styles from "../styles/Home.module.css";
+import { HeadLine } from "./HeadLine";
+import { Links } from "./Links";
+
+export function Main(props) {
+	return (
+		<main className={styles.main}>
+			<HeadLine page={props.page}>
+				<code className={styles.code}>pages/{props.page}.js</code>
+			</HeadLine>
+			<Links />
+		</main>
+	);
+}
